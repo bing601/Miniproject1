75e6b7112b74b771770d487980654d14b05ef689
bing601 <JIAneite21>
2018-10-11 20:41:02 -0400

    Fix : added bootstrap table

--- public/index.php
+++ public/index.php
@@ -16,7 +16,21 @@ class main  {
 class html {
     public static function generateTable($records) {
 
-        $html='<table>';
+        $html ='<!doctype html>';
+        $html.='<html lang="en">';
+            $html.='<head>';
+
+            $html.='<!-- Required meta tags -->';
+            $html.='<meta charset="utf-8">';
+            $html.='<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
+
+            $html.='<!-- Bootstrap CSS -->';
+            $html.='<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">';
+            $html.='</head>';
+
+            $html.='<body>';
+
+        $html.='<table class="table table-striped">';
 
         $count = 0;
         foreach ($records as $record) {
@@ -25,11 +39,13 @@ class html {
                 $fields = array_keys($array);
                 $values = array_values($array);
 
+                $html.='<thead class="thead-dark">';
                 $html.='<tr>';
                 foreach($fields as $field){
                     $html.='<th>'.htmlspecialchars($field).'</th>';
                 }
                 $html.='</tr>';
+                $html.='</thead>';
 
                 $html.='<tr>';
                 foreach($values as $value){
@@ -49,6 +65,13 @@ class html {
             $count++;
         }
         $html.='</table>';
+
+        $html.='<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>';
+        $html.='<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>';
+        $html.='<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>';
+
+        $html.='</body>';
+
         print $html;
     }
 }
